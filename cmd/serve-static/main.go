6886382ab8e3c6093717c1ebc9a3812b1c9d3c44
Todd Castilow <todd@leftslash.com>
2022-01-13 11:10:41 -0500

    added flags to serve-static

--- cmd/serve-static/main.go
+++ cmd/serve-static/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 
 	"github.com/leftslash/mux"
 )
 
 func main() {
-	addr := "127.0.0.1:8080"
+
+	addr := flag.String("addr", "127.0.0.1", "ip address on which to listen")
+	port := flag.Int("port", 8080, "tcp port on which to listen")
+	dir := flag.String("dir", ".", "directory of content to serve")
+	flag.Parse()
+
 	r := mux.NewRouter()
 	r.Use(mux.Logger(nil))
-	r.Handle("GET", "/*", http.FileServer(http.Dir(".")))
-	r.Run(addr)
+	r.Handle("GET", "/*", http.FileServer(http.Dir(*dir)))
+	r.Run(fmt.Sprintf("%s:%d", *addr, *port))
 }
